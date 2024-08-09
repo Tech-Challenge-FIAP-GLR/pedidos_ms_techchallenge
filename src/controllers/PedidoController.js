1120d1fcbe172318a8fe37247ba53e17850e271c
Raul Lima <raul.llima1@gmail.com>
2024-08-08 23:17:29 -0300

    criado o fluxo de consumo de mensageria

--- src/controllers/PedidoController.js
+++ src/controllers/PedidoController.js
@@ -2,93 +2,6 @@ const pedidoService = require("../services/PedidoService");
 const http = require('http');
 const amqp = require('amqplib');
 
-async function consume() {
-  try {
-    // Conectar ao servidor RabbitMQ com usuário e senha
-    const connection = await amqp.connect({
-      protocol: 'amqp',
-      hostname: 'rabbitmq',
-      port: 5672,
-      username: 'guest',
-      password: 'guest',
-      vhost: '/'
-    });
-
-    // Criar um canal
-    const channel = await connection.createChannel();
-
-    // Nome da fila que você quer consumir
-    const queuePedidosFinalizado = 'producao_ms_pedido_finalizado';
-    const queuePagamentosFail = 'pagamentos_ms_payment_fail';
-    const queuePagamentosSuccess = 'pagamentos_ms_pedido_success';
-
-    // Assegurar que a fila existe
-    await channel.assertQueue(queuePedidosFinalizado, {
-      durable: true
-    });
-
-    await channel.assertQueue(queuePagamentosFail, {
-      durable: true
-    });
-
-    await channel.assertQueue(queuePagamentosSuccess, {
-      durable: true
-    });
-
-    console.log(`[*] Esperando por mensagens na fila: ${queuePedidosFinalizado}. Para sair, pressione CTRL+C`);
-    
-    // Configurar o consumidor
-    channel.consume(queuePedidosFinalizado, (msg) => {
-      console.log(`[x] Recebido pedido finalizado: ${msg.content.toString()}`);
-      let objMessage = JSON.parse(msg.content);
-      // let objMessage = msg.content.toString();
-      if (msg !== null) {
-        console.log(`objMessage`, objMessage.orderStatus);
-        if(objMessage.orderStatus === 'FINALIZADO') {
-          console.log(`mensagem`, objMessage);
-          atualizaStatusPorMensagem(objMessage);
-        }
-        channel.ack(msg);
-      }
-    }, {
-      noAck: false
-    });
-
-    channel.consume(queuePagamentosFail, (msg) => {
-      console.log(`[x] Recebido pagamento falho: ${msg.content.toString()}`);
-      if (msg !== null) {
-        let objMessage = JSON.parse(msg.content);
-        console.log(`objMessage`, objMessage.orderStatus);
-        if(objMessage.orderStatus === 'CANCELADO') {
-          atualizaStatusPorMensagem(objMessage);
-        }
-        channel.ack(msg);
-      }
-    }, {
-      noAck: false
-    });
-
-    channel.consume(queuePagamentosSuccess, (msg) => {
-      console.log(`[x] Recebido pagamento sucesso: ${msg.content.toString()}`);
-      if (msg !== null) {
-        let objMessage = JSON.parse(msg.content);
-        console.log(`objMessage`, objMessage.orderStatus);
-        if(objMessage.orderStatus === 'PAGO') {
-          atualizaStatusPorMensagem(objMessage);
-        }
-        channel.ack(msg);
-      }
-    }, {
-      noAck: false
-    });
-
-  } catch (error) {
-    console.error('Erro ao consumir a fila:', error);
-  }
-}
-
-consume();
-
 exports.getAllPedidos = async (req, res) => {
   try {
     const pedidos = await pedidoService.getAllPedidos();
@@ -215,15 +128,3 @@ function postPagamentosMS(payload){
   makePost();
 }
 
-async function atualizaStatusPorMensagem(payload) {
-  console.log(payload,'paylaod')
-  try {
-    const atualizaStatus =  {
-     orderStatus: payload.orderStatus.toUpperCase()
-    }
-    const pedido = await pedidoService.updatePedido(payload.pedidoId, atualizaStatus);
-    pedido === null ? res.json({ message: 'Este Pedido não existe mais!' }) : res.json({ message: 'Pedido atualizado com sucesso' });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-}
